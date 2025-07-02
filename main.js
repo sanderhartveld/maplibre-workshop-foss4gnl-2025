64b8ebd6b43b19ea7d4892bde921c9a04e6b7a60
sanderhartveld <info@studiohartveld.nl>
2025-07-02 10:27:29 +0000

    Stap 5

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const protocol = new Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
-    container: 'mijnkaart', // container id
+   container: 'mijnkaart', // container id
    style: './assets/style.json',
    center: [5.66509, 51.96857], // starting position [lng, lat]
    zoom: 13 // starting zoom
